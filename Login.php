a3071251e660541e4099c28a22c82d17192a9e97
exam566 <SRIMCA\exam566@cs-u566.srimca.local>
2018-12-04 09:35:22 +0530

    First Commit

---
+++ Login.php
@@ -0,0 +1,41 @@
+<html>
+	<head>Login</head>
+	<body>
+		<form action="" method="post">
+  <table>
+    <tr>
+      <td><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td >Username</td>
+      <td><input name="Username" type="text"></td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td><input name="Password" type="password" ></td>
+    </tr>
+    <tr>
+      <td> </td>
+      <td><input name="Submit" type="submit" value="Login"></td>
+    </tr>
+  </table>
+</form>
+<?php session_start();
+if(isset($_POST['Submit'])){
+	$logins = array('Shehzad' => '123456','username1' => 'password1','username2' => 'password2');
+
+	$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+	$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+	if (isset($logins[$Username]) && $logins[$Username] == $Password){
+		$_SESSION['UserData']['Username']=$logins[$Username];
+		header("location:homepage.php");
+		exit;
+	} 
+	else {
+		$msg="<span style='color:red'>Invalid Login Details</span>";
+	}
+}
+?>
+</body>
+</html>
